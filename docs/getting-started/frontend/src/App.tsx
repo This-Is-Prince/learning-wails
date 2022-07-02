163f08a59634f759bec546d483b0127bbbd85769
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-07-02 12:06:58 +0530

    feat(style): tailwindcss added

--- docs/getting-started/frontend/src/App.tsx
+++ docs/getting-started/frontend/src/App.tsx
@@ -1,26 +1,17 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
 import {Greet} from "../wailsjs/go/main/App";
+import './index.css'
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
-
-    function greet() {
-        Greet(name).then(updateResultText);
+    
+    function greet(name:string) {
+        Greet(name).then((name)=>{
+            console.log(name);
+        });
     }
 
     return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
+        <div>
+            <h1 className="bg-red-500">Hello World</h1>
         </div>
     )
 }
